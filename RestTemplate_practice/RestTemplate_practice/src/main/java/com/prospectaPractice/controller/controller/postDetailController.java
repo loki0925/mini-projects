cd8da078268b94c329ae8b98c9e1a3ce21832330
Shreyash ahuja <98960409+loki0925@users.noreply.github.com>
2023-01-07 19:07:29 +0530

    Done

--- RestTemplate_practice/RestTemplate_practice/src/main/java/com/prospectaPractice/controller/controller/postDetailController.java
+++ RestTemplate_practice/RestTemplate_practice/src/main/java/com/prospectaPractice/controller/controller/postDetailController.java
@@ -1,5 +1,6 @@
 package com.prospectaPractice.controller.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prospectaPractice.controller.model.PostOfficeResponseBean;
+import com.prospectaPractice.controller.service.IPostService;
 
 @RestController
 @RequestMapping("/postal")
 public class postDetailController {
 
+	@Autowired
+	IPostService PostalServiceImpl;
+	
 	@RequestMapping(value ="/byCity", method = RequestMethod.GET,
 			consumes = MediaType.ALL_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
 	public PostOfficeResponseBean getPostalByCity(@RequestParam String city) {
 		
-		return null;
+		PostOfficeResponseBean postResponce ;
+
+		postResponce =PostalServiceImpl.fetchpostofficeDetailsByCity(city);
+		return postResponce;
 	}
 }
--- RestTemplate_practice/RestTemplate_practice/src/main/java/com/prospectaPractice/controller/service/IpostServiceImpl.java
+++ RestTemplate_practice/RestTemplate_practice/src/main/java/com/prospectaPractice/controller/service/IpostServiceImpl.java
@@ -1,16 +1,40 @@
 package com.prospectaPractice.controller.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.prospectaPractice.controller.model.PostOfficeBean;
 import com.prospectaPractice.controller.model.PostOfficeResponseBean;
 
 @Service("PostalServiceImpl")
 public class IpostServiceImpl implements IPostService{
 
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@Override
-	public 	PostOfficeResponseBean fetchpostofficeDetailsByCity(String city) {
+	public 	PostOfficeResponseBean fetchpostofficeDetailsByCity(String cityValue) {
 		
-		return null;
+		String url = "https://api.postalpincode.in/postoffice/{city}"
+				;
+		url = url.replace("{city}", cityValue);
+		
+		ResponseEntity<PostOfficeResponseBean[]> postalRespononseEntity=
+		
+		restTemplate.getForEntity(url,PostOfficeResponseBean[].class );
+		
+		PostOfficeResponseBean[] responseBeanArray=postalRespononseEntity.getBody();
+		
+		for(PostOfficeResponseBean responseBean:responseBeanArray) {
+			  List<PostOfficeBean> postOfficeListBean=responseBean.getPostOffice();		
+			  
+		}
+		
+		return responseBeanArray[0];
 		
 	}
 }
